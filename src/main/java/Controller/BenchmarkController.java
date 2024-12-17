63d1335c2bbab1a8af071487a709dfac348fd6c4
Luka Bagar <luka.bagar@studmail.w-hs.de>
2024-12-17 12:35:05 +0100

    init

---
+++ src/main/java/Controller/BenchmarkController.java
@@ -0,0 +1,232 @@
+package Controller;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class BenchmarkController
+{
+    public void initDB(int n)
+    {
+        //try Block um ClassNotFoundException und SQLException abzufangen
+        try
+        {
+            int anzahlRowBatch      = 1000;
+
+            //JDBC Treiber im Classpath hinzufügen
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            //Connection zur DB aufbauen
+            Connection con = DriverManager.getConnection(
+                    "jdbc:sqlserver://LAP-BOCHOLT233\\SQLEXPRESS:1433;database=DBI;encrypt=true;trustServerCertificate=true;useBulkCopyForBatchInsert=true;",
+                    "dbi",
+                    "dbi_pass"
+            );
+
+            con.setAutoCommit(false);
+
+            Statement stmt = con.createStatement();
+
+            //Falls Tabellen in DB noch nicht existieren, werden diese erstellt
+            //Befehl Create Table IF Not Exists gibt es nicht, daher IF Abfrage in SQL
+            stmt.execute("""
+                        IF(OBJECT_ID('branches') IS NULL)
+                        create table dbi.branches
+                        (branchid int not null,
+                        branchname char(20) not null,
+                        balance int not null,
+                        address char(72) not null,
+                        primary key (branchid) );""");
+
+            stmt.execute("""
+                        IF(OBJECT_ID('accounts') IS NULL)
+                        create table dbi.accounts
+                        ( accid int not null,
+                        name char(20) not null,
+                        balance int not null,
+                        branchid int not null,
+                        address char(68) not null,
+                        primary key (accid),
+                        foreign key (branchid) references branches );""");
+
+            stmt.execute("""
+                        IF(OBJECT_ID('tellers') IS NULL)
+                        create table dbi.tellers
+                        ( tellerid int not null,
+                        tellername char(20) not null,
+                        balance int not null,
+                        branchid int not null,
+                        address char(68) not null,
+                        primary key (tellerid),
+                        foreign key (branchid) references branches );""");
+
+            stmt.execute("""
+                        IF(OBJECT_ID('history') IS NULL)
+                        create table dbi.history
+                        ( accid int not null,
+                        tellerid int not null,
+                        delta int not null,
+                        branchid int not null,
+                        accbalance int not null,
+                        cmmnt char(30) not null,
+                        foreign key (accid) references accounts,
+                        foreign key (tellerid) references tellers,
+                        foreign key (branchid) references branches );
+                        """);
+
+
+            //Falls Werte vorhanden werden, werden diese aus den Tabellen gelöscht
+            stmt.execute("DELETE FROM dbi.history;");
+            stmt.execute("DELETE FROM dbi.accounts;");
+            stmt.execute("DELETE FROM dbi.tellers;");
+            stmt.execute("DELETE FROM dbi.branches;");
+
+            stmt.close();
+
+            System.out.println("Daten aus Datenbanken entfernt");
+
+            System.out.println("Start SQL Statements erstellen");
+
+            long startTimeMili = System.currentTimeMillis();
+
+            System.out.println("Start SQL Statements ausführen");
+
+            //Multiplikatoren von benötigte Tupel
+            int branchMultiplier    = 1;
+            int accountMultiplier   = 100000;
+            int tellerMultiplier    = 10;
+            int rowsCounterBatch    = 0;
+
+            //Tupel für branches hinzufügen
+            PreparedStatement preparedStatementBranch = con.prepareStatement("INSERT INTO dbi.branches (branchid, branchname, balance, address) VALUES (?, ?, ?, ?)");
+
+            for(int i = 1; i <= n * branchMultiplier; i++)
+            {
+                rowsCounterBatch++;
+
+                preparedStatementBranch.setInt      (1, i);
+                preparedStatementBranch.setString   (2, randomString(20));
+                preparedStatementBranch.setInt      (3, 0);
+                preparedStatementBranch.setString   (4, randomString(72));
+
+                preparedStatementBranch.addBatch();
+
+                if(rowsCounterBatch % anzahlRowBatch == 0)
+                {
+                    rowsCounterBatch = 0;
+                    preparedStatementBranch.executeBatch();
+                }
+            }
+
+            if(rowsCounterBatch > 0)
+            {
+                rowsCounterBatch = 0;
+                preparedStatementBranch.executeBatch();
+            }
+
+            preparedStatementBranch.close();
+
+            //Tupel für accounts hinzufügen
+            PreparedStatement preparedStatementAccounts = con.prepareStatement("INSERT INTO dbi.accounts (accid, name, balance, address, branchid) VALUES (?, ?, ?, ?, ?)");
+
+            for(int i = 1; i <= n * accountMultiplier; i++)
+            {
+                rowsCounterBatch++;
+
+                preparedStatementAccounts.setInt      (1, i);
+                preparedStatementAccounts.setString   (2, randomString(20));
+                preparedStatementAccounts.setInt      (3, 0);
+                preparedStatementAccounts.setString   (4, randomString(68));
+                preparedStatementAccounts.setInt      (5, randomBranchID(n));
+
+                preparedStatementAccounts.addBatch();
+
+                if(rowsCounterBatch % anzahlRowBatch == 0)
+                {
+                    rowsCounterBatch = 0;
+                    preparedStatementAccounts.executeBatch();
+                }
+            }
+
+            if(rowsCounterBatch > 0)
+            {
+                rowsCounterBatch = 0;
+                preparedStatementAccounts.executeBatch();
+            }
+
+            preparedStatementAccounts.close();
+
+            //Tupel für tellers hinzufügen
+            PreparedStatement preparedStatementTeller = con.prepareStatement("INSERT INTO dbi.tellers (tellerid, tellername, balance, address, branchid) VALUES (?, ?, ?, ?, ?)");
+
+            for(int i = 1; i <= n * tellerMultiplier; i++)
+            {
+                rowsCounterBatch++;
+
+                preparedStatementTeller.setInt      (1, i);
+                preparedStatementTeller.setString   (2, randomString(20));
+                preparedStatementTeller.setInt      (3, 0);
+                preparedStatementTeller.setString   (4, randomString(68));
+                preparedStatementTeller.setInt      (5, randomBranchID(n));
+
+                preparedStatementTeller.addBatch();
+
+                if(rowsCounterBatch % anzahlRowBatch == 0)
+                {
+                    rowsCounterBatch = 0;
+                    preparedStatementTeller.executeBatch();
+                }
+            }
+
+            if(rowsCounterBatch > 0)
+            {
+                preparedStatementTeller.executeBatch();
+            }
+
+            preparedStatementTeller.close();
+
+            long durationMili = System.currentTimeMillis() - startTimeMili;
+
+            System.out.println(durationMili / 1000 + "s wurden zum einfügen gebarucht");
+
+            //Verbindung schließen
+            con.close();
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int randomBranchID(int n)
+    {
+        //zufällige BranchID auswählen zwischen 1 und n
+        return randomInt(1, n);
+    }
+
+    private int randomInt(int low, int high)
+    {
+        //zufallszahl zwischen low und high auswählen
+        Random r = new Random();
+
+        if(low == high)
+            return low;
+
+        return r.nextInt(high-low) + low;
+    }
+
+    private String randomString(int length)
+    {
+        //zufälliger String mit der Länge length erstellen und ausgeben
+        String result = "";
+
+        for (int i = 0; i < length; i++)
+        {
+            result += (char) randomInt(97, 122);
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import Controller.BenchmarkController;
+
+public class Main
+{
+    /* Lokaler Rechner ohne Optimierung
+     * +------+------------+---------------------------+-------------------------+
+     * |  n   | Laufzeit   | Speicherverbrauch         | Speicherverbrauch (B)   |
+     * +------+------------+---------------------------+-------------------------+
+     * |  1   | 142s       | branches:  0,008 MB       | branches:  8.000 B      |
+     * |      |            | tellers:   0,008 MB       | tellers:   8.000 B      |
+     * |      |            | accounts:  10,563 MB      | accounts:  10.563.000 B |
+     * |      |            | history:   0,000 MB       | history:   0 B          |
+     * +------+------------+---------------------------+-------------------------+
+     * | 10   | 898s       | branches:  0,008 MB       | branches:  8.000 B      |
+     * |      |            | tellers:   0,016 MB       | tellers:   16.000 B     |
+     * |      |            | accounts:  105,578 MB     | accounts:  105.578.000 B|
+     * |      |            | history:   0,000 MB       | history:   0 B          |
+     * +------+------------+---------------------------+-------------------------+
+     *
+     * autocommit = false --> 387s
+     *
+     * batch alle Rows = n=1 -> 21s, n=10 -> Systemarbeitsspeicher überlaufen
+     * batch alle 10   Rows, SQL Befehle in ArrayList schreiben  = n=1 -> 9s, n=10 -> OpenJDK 64-Bit Server VM warning: INFO: os::commit_memory(0x00000000ad600000, 55574528, 0) failed; error='Die Auslagerungsdatei ist zu klein, um diesen Vorgang durchzuf�hren' (DOS error/errno=1455)
+     * batch alle 10   Rows, SQL Befehle nicht zwischenspeichern = n=1 -> 9s, n=10 -> 98s
+     * batch alle 100  Rows, SQL Befehle nicht zwischenspeichern = n=1 -> 7s, n=10 -> 53s
+     * batch alle 1000 Rows, SQL Befehle nicht zwischenspeichern = n=1 -> 6s, n=10 -> 51s --> scheint gut Wahl zu sein
+     * batch alle 2000 Rows, SQL Befehle nicht zwischenspeichern = n=1 -> 5s, n=10 -> 179s
+     * batch alle 1500 Rows, SQL Befehle nicht zwischenspeichern = n=1 -> 7s, n=10 -> 53s
+     * batch alle 1250 Rows, SQL Befehle nicht zwischenspeichern = n=1 -> 5s, n=10 -> 118s
+     *
+     * preparedStatement anstatt createStatement
+     * batch alle 1000 Rows, SQL Befehle nicht zwischenspeichern = n=1 -> 2s, n=10 -> 19s --> deutliche Performance Verbesserung
+     *
+     * optimiertes Programm DB auf lokalem Rechner:
+     * n=10 -> 19s
+     * n=20 -> 42s
+     * n=50 -> 118s
+     */
+
+    public static void main(String[] args)
+    {
+        System.out.println("Starting Benchmark Example");
+        BenchmarkController controller = new BenchmarkController();
+
+        controller.initDB(50);
+    }
+}
